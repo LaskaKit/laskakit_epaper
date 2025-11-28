2e941de158bb74fb0b1603a269d7347e335f4211
Patrik Novak <novakpatrik.99@protonmail.com>
2025-11-28 14:54:33 +0100

    Add new display GDEM102F91.
    - here we use GxEPD2 library underneath. We integrate it into our
      interface.

---
+++ src/laskakit_GDEM102F91.hpp
@@ -0,0 +1,41 @@
+
+
+#include <GxEPD2_4C.h>
+
+#include "epdbus.hpp"
+
+#define ENABLE_GxEPD2_GFX 0
+
+namespace LaskaKit::Epaper {
+    class GDEM102F91 {
+    public:
+        static constexpr size_t WIDTH = 960;
+        static constexpr size_t HEIGHT = 640;
+    private:
+        // GxEPD2_4C<GxEPD2_1160c_GDEY116F51, GxEPD2_1160c_GDEY116F51::HEIGHT / 2> display;
+        GxEPD2_4C<GxEPD2_1160c_GDEY116F51, GxEPD2_1160c_GDEY116F51::HEIGHT> display;
+
+    public:
+        GDEM102F91(const EPDBusSettings& settings)
+            : display(GxEPD2_1160c_GDEY116F51(settings.cs, settings.dc, settings.reset, settings.busy))
+        {
+            SPI.begin(settings.sck, -1, settings.mosi);
+            display.init();
+        }
+
+        ~GDEM102F91()
+        {
+            SPI.end();
+        }
+
+        void fullUpdate()
+        {
+            display.display();
+        }
+
+        void drawPixel(int16_t x, int16_t y, uint32_t color)
+        {
+            display.drawPixel(x, y, color);
+        }
+    };
+}
