9b641e9c1dd682866c4bf4085d403d97fdcb669b
Patrik Novak <novakpatrik.99@protonmail.com>
2025-11-28 13:24:42 +0100

    Cleanup GDEY075T7, convert to epdbus.

--- src/laskakit_GDEY075T7.hpp
+++ src/laskakit_GDEY075T7.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <laskakit_epaper.hpp>
+#include "laskakit_epaper.hpp"
 
-#include "raw.hpp"
-
+#include "epdbus.hpp"
 
 namespace LaskaKit::Epaper {
     class GDEY075T7 : public Display
@@ -15,6 +14,12 @@ namespace LaskaKit::Epaper {
         uint8_t* bufferOld;
         uint8_t* bufferNew;
 
+        static const uint8_t lut_vcom[42];
+        static const uint8_t lut_ww[42];
+        static const uint8_t lut_bw[42];
+        static const uint8_t lut_wb[42];
+        static const uint8_t lut_bb[42];
+
     public:
         GDEY075T7()
         {
@@ -24,19 +29,10 @@ namespace LaskaKit::Epaper {
                 Serial.println("error allocating ram");
             }
 
-            delay(500);
-            pinMode(PIN_EPD_BUSY, INPUT);
-            pinMode(PIN_EPD_RST, OUTPUT);
-            pinMode(PIN_EPD_DC, OUTPUT);
-            pinMode(PIN_EPD_CS, OUTPUT);
-            //SPI
-            SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0)); 
-            SPI.begin (PIN_EPD_SCL, -1, PIN_EPD_SDA, PIN_EPD_CS);
             this->setupBuffer();
         }
 
         ~GDEY075T7() {
-            SPI.end();
             if (this->bufferOld) {
                 free(this->bufferOld);
             }
@@ -85,38 +81,53 @@ namespace LaskaKit::Epaper {
                 }
             }
         }
-        
+
 
         //4 grayscale demo function
         /********Color display description
              white  gray1  gray2  black
         0x10|  01     01     00     00
         0x13|  01     00     01     00
-                                        ****************/
+        ****************/
         void writeBufferToScreen() {
-            // Old data (0x10)
-            EPD_W21_WriteCMD(0x10);
-            for (size_t i = 0; i < 48000; i++) {
-                EPD_W21_WriteDATA(this->bufferOld[i]);
-            }
-            
-            // New data (0x13)
-            EPD_W21_WriteCMD(0x13);
-            for (size_t i = 0; i < 48000; i++) {
-                EPD_W21_WriteDATA(this->bufferNew[i]);
-            }
-            
-            lut(); // Trigger display refresh
+            EPDBus::_WriteCmdData(0x10, this->bufferOld, 48000);
+            EPDBus::_WriteCmdData(0x13, this->bufferNew, 48000);
         }
 
         void fullUpdate()
         {
-            EPD_init_4Gray();
+            EPDBus::BeginTransaction();
+            EPDBus::Reset();
+
+            EPDBus::WriteCmdData(0x06, {0x17, 0x17, 0x28, 0x17});  // booster soft start
+            EPDBus::WriteCmd(0x04);  // power on
+            EPDBus::DelayMs(100);
+            EPDBus::BusyPoll(0x71);  // waiting for the electronic paper IC to release the idle signal
+            EPDBus::WriteCmdData(0x00, {0xBF});
+            EPDBus::WriteCmdData(0x30, {0x06});
+            EPDBus::WriteCmdData(0x61, {0x03, 0x20, 0x01, 0xE0});  // set resolution
+            EPDBus::WriteCmdData(0x15, {0x00});
+            EPDBus::WriteCmdData(0x60, {0x22});  // TCON setting
+            EPDBus::WriteCmdData(0x82, {0x12});  // vcom_DC setting
+            EPDBus::WriteCmdData(0x50, {0x10, 0x07});  // vcom and data interval setting, 10:KW(0--1)  21:KW(1--0)
+
+            // write buffers to screen
             this->writeBufferToScreen();
-            EPD_W21_WriteCMD(0x12);   //DISPLAY REFRESH   
-            driver_delay_xms(100);    //!!!The delay here is necessary, 200uS at least!!!     
-            lcd_chkstatus();
-            EPD_sleep();
+
+            // write some magic lookup tables
+            this->lut();
+
+            // display refresh
+            EPDBus::WriteCmd(0x12);  // display refresh
+            EPDBus::DelayMs(100);  // delay here is necessary, 200uS at least!!!
+            EPDBus::BusyPoll(0x71);
+
+            // epd sleep
+            EPDBus::WriteCmdData(0x50, {0xF7});  // vcom and data interval setting
+            EPDBus::WriteCmd(0x02);  // power off
+            EPDBus::BusyPoll(0x71);
+            EPDBus::WriteCmdData(0x07, {0xA5});
+            EPDBus::EndTransaction();
         }
 
         void drawPixel(int x, int y, uint8_t color)
@@ -139,5 +150,72 @@ namespace LaskaKit::Epaper {
             // this->buffer[pos] = color;
             // printf("%lu\n", shift);
         }
+
+    private:
+        void lut()
+        {
+            EPDBus::_WriteCmdData(0x20, lut_vcom, 42);  // vcom
+            EPDBus::_WriteCmdData(0x21, lut_ww, 42);  // red not use
+            EPDBus::_WriteCmdData(0x22, lut_bw, 42);  // bw r
+            EPDBus::_WriteCmdData(0x23, lut_wb, 42);  // wb r
+            EPDBus::_WriteCmdData(0x24, lut_bb, 42);  // bb b
+            EPDBus::_WriteCmdData(0x25, lut_ww, 42);  // vcom
+        }
+    };
+
+
+    // 0~3 gray
+    const uint8_t GDEY075T7::lut_vcom[42] = {
+        0x00, 0x0A, 0x00, 0x00, 0x00, 0x01,
+        0x60, 0x14, 0x14, 0x00, 0x00, 0x01,
+        0x00, 0x14, 0x00, 0x00, 0x00, 0x01,
+        0x00, 0x13, 0x0A, 0x01, 0x00, 0x01,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+
+     // R21
+    const uint8_t GDEY075T7::lut_ww[42] = {
+        0x40, 0x0A, 0x00, 0x00, 0x00, 0x01,
+        0x90, 0x14, 0x14, 0x00, 0x00, 0x01,
+        0x10, 0x14, 0x0A, 0x00, 0x00, 0x01,
+        0xA0, 0x13, 0x01, 0x00, 0x00, 0x01,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+
+    // R22H  r
+    const uint8_t GDEY075T7::lut_bw[42] = {
+        0x40, 0x0A, 0x00, 0x00, 0x00, 0x01,
+        0x90, 0x14, 0x14, 0x00, 0x00, 0x01,
+        0x00, 0x14, 0x0A, 0x00, 0x00, 0x01,
+        0x99, 0x0C, 0x01, 0x03, 0x04, 0x01,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+
+    // R23H  w
+    const uint8_t GDEY075T7::lut_wb[42] = {
+        0x40, 0x0A, 0x00, 0x00, 0x00, 0x01,
+        0x90, 0x14, 0x14, 0x00, 0x00, 0x01,
+        0x00, 0x14, 0x0A, 0x00, 0x00, 0x01,
+        0x99, 0x0B, 0x04, 0x04, 0x01, 0x01,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+
+    // R24H  b
+    const uint8_t GDEY075T7::lut_bb[42] = {
+        0x80, 0x0A, 0x00, 0x00, 0x00, 0x01,
+        0x90, 0x14, 0x14, 0x00, 0x00, 0x01,
+        0x20, 0x14, 0x0A, 0x00, 0x00, 0x01,
+        0x50, 0x13, 0x01, 0x00, 0x00, 0x01,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
     };
 }
