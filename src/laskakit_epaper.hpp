b5aaec5a6f6fb413bfd6ede93e897b6db673e169
Patrik Novak <novakpatrik.99@protonmail.com>
2025-12-05 19:20:53 +0100

    Update epaper interface.
    Ditch the abstract base class because we are using templates
    and do not inherit from this. If concrete display class does not have
    needed methods and variables it would not compile.
    
    The interface is designed to take color in RGB565 format for greater
    compatibility. Base colors in RGB565 are defined.
    
    Cleanup.

--- src/laskakit_epaper.hpp
+++ src/laskakit_epaper.hpp
@@ -1,84 +1,57 @@
 #pragma once
 
 #include <Arduino.h>
-// #include <Adafruit_GFX.h>
-#include <memory>
-
 
 namespace LaskaKit::Epaper {
-    enum class ColorType {
-        BW,  // black and white
-        G4,  // four shades of gray
-        G8,  // eight shades of gray
-        RBW, // red, black, white
-        YBW, // yellow, black, white
-        C4,  // four colors (black, white, red, yellow)
-        C7,  // seven colors
-    };
+enum class ColorType {
+  BW,  // black and white
+  G4,  // four shades of gray
+  G8,  // eight shades of gray
+  RBW, // red, black, white
+  YBW, // yellow, black, white
+  C4,  // four colors (black, white, red, yellow)
+  C7,  // seven colors
+};
 
-    enum class Color {
-        BLACK,
-        WHITE,
-        YELLOW,
-        RED,
-        GRAY_0,
-        GRAY_1,
-        GRAY_2,
-        GRAY_3,
-        GRAY_4,
-        GRAY_5,
-        GRAY_6,
-        GRAY_7,
-    };
+enum class RGB565 : uint16_t {
+  BLACK = 0x0000,
+  WHITE = 0xFFFF,
+  LIGHT_GRAY = 0xAD43,
+  LIGHT_GRAY2 = 0xD6BA,
+  LIGHT_GRAY3 = 0x8430,
+  DARK_GRAY = 0x528A,
+  DARK_GRAY2 = 0x2945,
+  DARK_GRAY3 = 0x6B4D,
+  RED = 0xF800,
+  YELLOW = 0xFFE0,
+  GREEN = 0x07E0,
+  BLUE = 0x001F,
+  ORANGE = 0xFBE0,
+  PURPLE = 0x7814,
+};
 
-    enum class DisplayRotation
-    {
-        NO_ROTATION,
-        RIGHT_90,
-        LEFT_90,
-        RIGHT_180,
-        LEFT_180,
-        RIGHT_270,
-        LEFT_270
-    };
-
-    // common interface for all displays
-    class Display
-    {
-    public:
-        // virtual size_t width() = 0;
-        // virtual size_t height() = 0;
-        // virtual void on() = 0;
-        // virtual void off() = 0;
-        virtual void fullUpdate() = 0;
-        // virtual void setRotation(DisplayRotation rot) = 0;
-        // virtual void fillScreen(int color) = 0;
-        virtual void drawPixel(int x, int y, uint8_t color) = 0;
-        // virtual void drawText(int x, int y, const String& text, uint8_t color) = 0;
-        // virtual void drawCenteredText(int x, int y, const String& text, uint8_t color) = 0;
-        // virtual void fillRect(int x, int y, int width, int heitht) = 0;
-        virtual ~Display() {}
-    };
-
-    // class DisplayGFX : public Adafruit_GFX
-    // {
-    // private:
-    //     std::unique_ptr<Display> display;
-    // public:
-    //     DisplayGFX(std::unique_ptr<Display>& display)
-    //         : Adafruit_GFX(display->width(), display->height()),
-    //           display(std::move(display))
-    //     {
-    //     }
-
-    //     void drawPixel(int16_t x, int16_t y, uint16_t color)
-    //     {
-    //         this->display->drawPixel(x, y, color);
-    //     };
-
-    //     void fullUpdate()
-    //     {
-    //         this->display->fullUpdate();
-    //     }
-    // };
+constexpr bool operator==(uint16_t lhs, RGB565 rhs) noexcept {
+  return lhs == static_cast<uint16_t>(rhs);
 }
+
+const char *colorTypeToCStr(ColorType colorType) {
+  switch (colorType) {
+  case ColorType::BW:
+    return "BW";
+  case ColorType::G4:
+    return "4G";
+  case ColorType::G8:
+    return "8G";
+  case ColorType::RBW:
+    return "RBW";
+  case ColorType::YBW:
+    return "YBW";
+  case ColorType::C4:
+    return "4C";
+  case ColorType::C7:
+    return "7C";
+  }
+  return "";
+}
+
+} // namespace LaskaKit::Epaper
