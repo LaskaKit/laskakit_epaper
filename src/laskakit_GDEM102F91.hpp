0ea388ea78caa97c1e55c81e108ac78ddebedd92
Patrik Novak <novakpatrik.99@protonmail.com>
2026-03-20 09:49:01 +0100

    Fix GDEM102F91

--- src/laskakit_GDEM102F91.hpp
+++ src/laskakit_GDEM102F91.hpp
@@ -2,6 +2,7 @@
 
 #include <GxEPD2_4C.h>
 
+#include "epdbus.hpp"
 #include "laskakit_GDEY116F51.hpp"
 
 #define ENABLE_GxEPD2_GFX 0
@@ -9,7 +10,10 @@
 namespace LaskaKit::Epaper {
 
 class GDEM102F91 : public GDEY116F51 {
-
+public:
+    GDEM102F91(const EPDBusSettings& settings)
+        : GDEY116F51(settings)
+    {}
 };
 
 }
