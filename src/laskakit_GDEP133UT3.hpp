87ed2250a835598ab034b6a90ec9eebcafff470f
Patrik Novák <novakpatrik.99@protonmail.com>
2026-05-22 15:30:02 +0200

    Use epdiy driver.
    This fixes the 'tearing' issues.
    Downside is that it can not be used with
    sensors currently.

--- src/laskakit_GDEP133UT3.hpp
+++ src/laskakit_GDEP133UT3.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <FastEPD.h>
+#include <epdiy.h>
 
 #include "laskakit_epaper.hpp"
 #include "epdbus.hpp"
+
 
 namespace LaskaKit::Epaper {
 
@@ -11,59 +12,39 @@ namespace LaskaKit::Epaper {
     public:
         static constexpr size_t WIDTH = 1600;
         static constexpr size_t HEIGHT = 1200;
-        static constexpr ColorType COLORTYPE = ColorType::G8;
+        static constexpr ColorType COLORTYPE = ColorType::G16;
         static constexpr const char* NAME = "GDEP133UT3";
-
     private:
-        static FASTEPD epaper;
+        EpdiyHighlevelState hl;
+        uint8_t* fb;
 
     public:
         GDEP133UT3(const EPDBusSettings& settings) {}
 
         bool init()
         {
-            epaper.initPanel(BB_PANEL_EPDIY_V7);
-            epaper.setPanelSize(1600, 1200);
-            epaper.setMode(BB_MODE_4BPP);
+            epd_init(&epd_board_v7, &ED133UT2, EPD_LUT_64K);
+            epd_set_vcom(300);
+            hl = epd_hl_init(EPD_BUILTIN_WAVEFORM);
+            fb = epd_hl_get_framebuffer(&hl);
             return true;
         }
 
         void fullUpdate()
         {
-            epaper.fullUpdate();
+            epd_poweron();
+            epd_clear();
+            epd_hl_update_screen(&hl, MODE_EPDIY_WHITE_TO_GL16, epd_ambient_temperature());
+            epd_poweroff();
         }
 
-        void drawPixel(int16_t x, int16_t y, uint16_t color)
+        void drawPixel(int16_t x, int16_t y, uint8_t color)
         {
-            uint8_t col = 0xF;
-            if (color == RGB565::WHITE) {
-                col = 0xF;
+            if (color > 0xF) {
+                return;
             }
-            if (color == RGB565::BLACK) {
-                col = 0x0;
-            }
-            if (color == RGB565::LIGHT_GRAY) {
-                col = 0xB;
-            }
-            if (color == RGB565::LIGHT_GRAY2) {
-                col = 0x9;
-            }
-            if (color == RGB565::LIGHT_GRAY3) {
-                col = 0x8;
-            }
-            if (color == RGB565::DARK_GRAY) {
-                col = 0x6;
-            }
-            if (color == RGB565::DARK_GRAY2) {
-                col = 0x4;
-            }
-            if (color == RGB565::DARK_GRAY3) {
-                col = 0x2;
-            }
-            epaper.drawPixel(x, y, col);
+            // epdiy uses 0->255
+            epd_draw_pixel(x, y, color * 17, fb);
         }
     };
-
-    FASTEPD GDEP133UT3::epaper;
-
 } // LaskaKit::Epaper
